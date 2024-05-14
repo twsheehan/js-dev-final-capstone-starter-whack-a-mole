a21b279522b113b0b27d43240faab1b72a77bb62
Thomas Sheehan <tsheehan@indeed.com>
2024-05-14 14:55:12 +0900

    US-05: Timer

--- src/index.js
+++ src/index.js
@@ -180,8 +180,10 @@ function clearScore() {
  *
  */
 function updateTimer() {
-  // TODO: Write your code here.
-  // hint: this code is provided to you in the instructions.
+  if (time > 0) {
+    time--;
+    timerDisplay.textContent = String(time);
+  }
 
   return time;
 }
@@ -193,8 +195,7 @@ function updateTimer() {
  *
  */
 function startTimer() {
-  // TODO: Write your code here
-  // timer = setInterval(updateTimer, 1000);
+  timer = setInterval(updateTimer, 1000);
   return timer;
 }
 
