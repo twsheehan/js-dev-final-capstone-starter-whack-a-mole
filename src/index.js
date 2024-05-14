a4796770714f1ab942dd81a750ad687bb85b1f50
Thomas Sheehan <tsheehan@indeed.com>
2024-05-14 13:34:20 +0900

    US-03 - Game flow

--- src/index.js
+++ src/index.js
@@ -95,7 +95,7 @@ function chooseHole(holes) {
  *
  */
 function gameOver() {
-  // TODO: Write your code here
+  return time > 0 ? showUp() : stopGame();
 }
 
 /**
@@ -108,8 +108,8 @@ function gameOver() {
  *
  */
 function showUp() {
-  let delay = 0; // TODO: Update so that it uses setDelay()
-  const hole = 0; // TODO: Update so that it use chooseHole()
+  let delay = setDelay(difficulty);
+  const hole = chooseHole(holes);
   return showAndHide(hole, delay);
 }
 
@@ -122,13 +122,13 @@ function showUp() {
  *
  */
 function showAndHide(hole, delay) {
-  // TODO: call the toggleVisibility function so that it adds the 'show' class.
+  toggleVisibility(hole);
 
   const timeoutID = setTimeout(() => {
-    // TODO: call the toggleVisibility function so that it removes the 'show' class when the timer times out.
+    toggleVisibility(hole);
 
     gameOver();
-  }, 0); // TODO: change the setTimeout delay to the one provided as a parameter
+  }, delay);
   return timeoutID;
 }
 
@@ -139,7 +139,7 @@ function showAndHide(hole, delay) {
  *
  */
 function toggleVisibility(hole) {
-  // TODO: add hole.classList.toggle so that it adds or removes the 'show' class.
+  hole.classList.toggle("show");
 
   return hole;
 }
@@ -253,8 +253,8 @@ function stopGame() {
  *
  */
 function startGame() {
-  //setDuration(10);
-  //showUp();
+  setDuration(10);
+  showUp();
   return "game started";
 }
 
